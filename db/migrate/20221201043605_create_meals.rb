4556649b40af63503a6d0977b318810a3e05b512
jihaneh <jijih@hotmail.fr>
2022-12-02 22:20:12 +0100

    Update  Meal model

--- db/migrate/20221201043605_create_meals.rb
+++ db/migrate/20221201043605_create_meals.rb
@@ -1,12 +1,11 @@
 class CreateMeals < ActiveRecord::Migration[7.0]
   def change
     create_table :meals do |t|
-      t.string :name, default: ''
-      t.string :price, default: ''
+      t.string :title, null: false
+      t.float :price, null: false
       t.text :description, default: ''
-      t.text :photo, default: ''
-      t.string :rating, default: ''
-      t.string :user_ratings_total, default: ''
+      t.text :image_url, default: ''
+      t.boolean :desactivate, default: false
       t.belongs_to :restaurant, null: false, foreign_key: true
       t.timestamps
     end
