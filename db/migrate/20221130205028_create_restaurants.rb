9e2a404c99df92b1cfae096a9e10bac9c8e0fa63
jihaneh <jijih@hotmail.fr>
2022-12-02 22:19:49 +0100

    Update Restaurant model

--- db/migrate/20221130205028_create_restaurants.rb
+++ db/migrate/20221130205028_create_restaurants.rb
@@ -1,18 +1,18 @@
 class CreateRestaurants < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurants do |t|
-      t.string :name, default: ''
-      t.string :description, default: ''
-      t.string :location, default: ''
-      t.string :location_lat, default: ''
-      t.string :location_lng, default: ''
-      t.string :user_ratings_total, default: ''
-      t.string :opening_hours, default: ''
-      t.string :rating, default: ''
-      t.string :payment, default: ''
-      t.text :itinerary, default: ''
-      t.text :image, default: ''
-
+      t.string :name, null: false
+      t.string :adress, null: false
+      t.string :stree_adress, null: false
+      t.string :city, null: false
+      t.string :country, null: false
+      t.string :postal_code,default: ''
+      t.string :region, default: ''
+      t.string :latitude, default: ''
+      t.string :longitude, default: ''
+      t.string :phone_number, default: ''
+      t.string :currency_code, default: ''
+    
       t.timestamps
     end
   end
