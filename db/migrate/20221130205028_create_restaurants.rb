500f4d8d54528fb2c992bc79c5142758401252cc
jihaneh <jijih@hotmail.fr>
2022-12-03 07:36:45 +0100

    Update Restaurant model (adress to address)

--- db/migrate/20221130205028_create_restaurants.rb
+++ db/migrate/20221130205028_create_restaurants.rb
@@ -2,8 +2,8 @@ class CreateRestaurants < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurants do |t|
       t.string :name, null: false
-      t.string :adress, null: false
-      t.string :street_adress, null: false
+      t.string :address, null: false
+      t.string :street_address, null: false
       t.string :city, null: false
       t.string :country, null: false
       t.string :postal_code,default: ''
