4b06ba17acf3d943acfce6f3d9f04597b6066303
jihaneh <jijih@hotmail.fr>
2022-12-03 15:32:33 +0100

    Add Dashboard Meal and Category link

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,11 @@
 class DashboardController < ApplicationController
   def index; end
+
+  def meals
+    @meals = Meal.where(desactivate: false)
+  end
+
+  def categories
+    @categories = Category.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :categories
+  #resources :categories
   root "dashboard#index"
+  get '/meals', to: 'dashboard#meals'
+  get '/categories', to: 'dashboard#categories'
+
 
   resources :restaurants do
       resources :meals
